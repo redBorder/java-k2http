ac558a7ff2213e5eb376ecdc3efd39ba916a3515
Diego Fernández <bigomby@gmail.com>
2015-07-23 14:03:37 +0200

    First attempt to read from multiple topics. - FIXME: Every instance is being registered one time for each topic.

--- src/main/java/net/redborder/k2http/util/ConfigData.java
+++ src/main/java/net/redborder/k2http/util/ConfigData.java
@@ -1,18 +1,25 @@
 package net.redborder.k2http.util;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ConfigData {
     private static final String CONFIG_FILE_PATH = "/opt/rb/etc/k2http/config.yml";
     private static final ConfigFile configFile = new ConfigFile(CONFIG_FILE_PATH);
+    private static final List<String> topics = new LinkedList<>();
 
     private ConfigData() {
     }
+
 
     public static String getZkConnect() {
         return configFile.getOrDefault("zk_connect", "127.0.0.1:2181");
     }
 
-    public static String getTopic() {
-        return configFile.getOrDefault("topic", "default_topic");
+    public static List<String> getTopics() {
+        topics.add("default_topic_1");
+        topics.add("default_topic_2");
+        return configFile.getOrDefault("topic", topics);
     }
 
     public static Integer getThreadNum() {
@@ -24,7 +31,7 @@ public class ConfigData {
     }
 
     public static String getEndPoint() {
-        return configFile.getOrDefault("endpoint", "http://127.0.0.1/");
+        return configFile.getOrDefault("endpoint", "http://127.0.0.1:8080/");
     }
 
     public static void reload(){
@@ -34,7 +41,7 @@ public class ConfigData {
     public static String currentConfig(){
         return  "{\"type\":\"config\"," +
                 " \"zkConnect\":" + getZkConnect() + "," +
-                " \"topic\":" + getTopic() + "," +
+                " \"topic\":" + getTopics() + "," +
                 " \"httpThreads\":" + getThreadNum() +
                 " \"maxQueueSize\":" + getMaxQueueSize() +
                 " \"endPoint\":" + getEndPoint() +
